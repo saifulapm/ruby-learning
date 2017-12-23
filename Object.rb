7b80995dc8fb47a68dd302d5113cebc401611082
Saiful Islam <saiful.apm@gmail.com>
2017-12-23 08:50:23 +0600

    Object Processing

--- Object.rb
+++ Object.rb
@@ -4,16 +4,63 @@
 ### Public Instance Methods
 
 # obj !~ other → true or false
+#Returns true if two objects do not match (using the =~ method), otherwise false. Regular Expression Matching
+obj = 'Me'
+obj !~ /Me/ # => false
 
 # obj <=> other → 0 or nil
+# Returns 0 if obj and other are the same object or obj == other, otherwise nil.
+# Your implementation of <=> should return one of the following values: -1, 0, 1 or nil. -1 means self is smaller than other. 0 means self is equal to other. 1 means self is bigger than other. Nil means the two values could not be compared.
+other = obj.dup
+obj <=> other  # => 0
+obj <=> 'saiful' # => -1
+11 <=> 10 # => 1
 
-# obj === other → true or false
 
-# obj =~ other → nil
+# obj === other → true or false // case equality operator.. Case Equality 
+obj === other  # => true
+(1..5) === 3 # => true
+(1..5) === 6 # => false
+Integer === 42 # => true
+Integer === 'fourtytwo' # => false
+/ell/ === 'Hello' # => true
+/ell/ === 'Foobar' # => false
+/test/ === "this is a test" # => true
+# === is used in case statements as such:
+case 'test'
+when /blah/
+  "Blach"
+when /test/
+  "Test"
+else
+  "Fail"
+end # => "Test"
+
+# Is the same as
+vari = 'test'
+if /blah/ === vari
+  "Blash"
+elsif /test/ === vari
+  "Test"
+else
+  "Fail"
+end # => "Test"
+
+
+# obj =~ other → nil //Pattern Match
+'Saiful'.=~ /Saiful/ # => 0
+'Me' =~ /ABC/ # => nil
 
 # class → class
+# This method must always be called with an explicit receiver, as class is also a reserved word in Ruby.
+1.class  # => Integer
+self.class  # => Object
+'Saiful'.class  # => String
+/Me/.class  # => Regexp
 
 # clone(freeze: true) → an_object
+
+
 
 # define_singleton_method(symbol, method) → symbol 
 # define_singleton_method(symbol) { block } → symbol
